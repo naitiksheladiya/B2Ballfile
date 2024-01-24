7b166b17928d853d8668db7fad05fbecdec1bbef
HemangKatariya <Hemangk50@gmail.com>
2024-01-24 19:18:08 +0530

    catalog

--- frontend/src/catalog.js
+++ frontend/src/catalog.js
@@ -1,22 +1,23 @@
-import UserLayout from './Layout'
+import UserLayout from './Layout';
 import '../src/css/catalog.css';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export default function catalog() {
+export default function Catalog() {
+    const [businessName, setBusinessName] = useState('');
+    const [editorData, setEditorData] = useState('');
+    const [isPopupOpen, setPopupOpen] = useState(false);
 
     const handleReady = (editor) => {
-        // Add your custom CSS class to the CKEditor element
-        const editorElement = editor.ui.view.editable.element;
-        editorElement.classList.add('ckbox');
-
-        // You can store the "editor" and use it when needed.
+        // You can store the "editor" and use when it is needed.
         console.log('Editor is ready to use!', editor);
     };
 
     const handleChange = (event, editor) => {
         console.log(event);
+        const data = editor.getData();
+        setEditorData(data);
     };
 
     const handleBlur = (event, editor) => {
@@ -27,23 +28,74 @@ export default function catalog() {
         console.log('Focus.', editor);
     };
 
-    // Cleanup function (optional) to remove event listeners or do other cleanup.
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Additional logic for handling the submit can be added here
 
+        // Open the popup
+        setPopupOpen(true);
+    };
+
+    useEffect(() => {
+        // You can perform any additional logic here when businessName or editorData changes
+    }, [businessName, editorData]);
     return (
         <>
             <UserLayout />
-            <div className='container layber'>
-                <div className="App">
-                    <CKEditor
-                        editor={ClassicEditor}
-                        data="<p>Hello from CKEditor&nbsp;5!</p>"
-                        onReady={handleReady}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        onFocus={handleFocus}
-                    />
+
+            <div className='main layber'>
+                <div className='box_ps'>
+                    <form className="login100-form_0111">
+                        <span className="login100-form-title_034 p-b-43">
+                            ADD CATALOG
+                        </span>
+
+
+                        <div className='wrap-input100 validate-input'>
+                            <input class="input100" type="text"
+                                name="name" placeholder='PRODUCT NAME' />
+                            <span class="focus-input100"></span>
+                        </div>
+                        <br />
+
+                        <CKEditor
+                            editor={ClassicEditor}
+                            data={editorData}
+                            onReady={handleReady}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            onFocus={handleFocus}
+                        />
+                        <br />
+
+                        <div className='wrap-input100 validate-input'>
+                            <input class="input100" type="number"
+                                name="price" placeholder='PRODUCT PRICE' />
+                            <span class="focus-input100"></span>
+                        </div>
+
+
+                        <div className="container-login100-form-btn">
+                            <button className="login100-form-btn_confrom01 subtext">
+                                SUBMIT
+                            </button>
+                        </div>
+
+                        {isPopupOpen && (
+                            <div className='popup'>
+                                {/* Add your popup content here */}
+                                <div className='popup-content'>
+                                    <span className='close-popup' onClick={() => setPopupOpen(false)}>
+                                        ×
+                                    </span>
+                                    <p>Your popup content goes here.</p>
+                                </div>
+                            </div>
+                        )}
+                    </form>
                 </div>
             </div>
+
         </>
-    )
+    );
 }
