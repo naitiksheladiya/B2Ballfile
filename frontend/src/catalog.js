490124a65ead534e3470415064c9e8164aa98cb4
HemangKatariya <Hemangk50@gmail.com>
2024-01-31 12:10:01 +0530

    formic

--- frontend/src/catalog.js
+++ frontend/src/catalog.js
@@ -1,178 +1,140 @@
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import UserLayout from './Layout';
 import '../src/css/catalog.css';
-import React, { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useNavigate } from "react-router-dom";
+import Select from 'react-select';
 
-
-
-export default function Catalog() {
-    const [businessName, setBusinessName] = useState('');
-    const [editorData, setEditorData] = useState('');
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [selectedCategoriesList, setSelectedCategoriesList] = useState([]); // State to store selected categories
-    const [selectedColor, setSelectedColor] = useState('');
-    const [selectedColorsList, setSelectedColorList] = useState([]);// State to store selected color
-    const [selectedImage, setSelectedImage] = useState(null); // State to store selected image file
-    const [imageUrl, setImageUrl] = useState(''); // State to store the URL of the selected image
-
-
-    useEffect(() => {
-        // You can perform any additional logic here when businessName or editorData changes
-    }, [businessName, editorData, selectedCategory, selectedColor, selectedImage]);
-
-    const handleReady = (editor) => {
-        console.log('Editor is ready to use!', editor);
-    };
-
-    const handleChange = (event, editor) => {
-        console.log(event);
-        const data = editor.getData();
-        setEditorData(data);
-    };
-
-    const handleCategoryChange = (event) => {
-        const category = event.target.value;
-        setSelectedCategory(category);
-
-        // Add the selected category to the list if it's not already present
-        if (!selectedCategoriesList.includes(category)) {
-            setSelectedCategoriesList([...selectedCategoriesList, category]);
-        }
-    };
-
-    const handleColorChange = (event) => {
-        const color = event.target.value
-        setSelectedColor(color);
-
-        if (!selectedColorsList.includes(color)) {
-            setSelectedColorList([...selectedColorsList, color]);
-        }
-    };
-
-    const handleImageChange = (event) => {
-        const file = event.target.files[0];
-        setSelectedImage(file);
-
-        const reader = new FileReader();
-        reader.onloadend = () => {
-            setImageUrl(reader.result);
-        };
-        reader.readAsDataURL(file);
-    };
-
-    const handleRemoveCategory = (indexToRemove) => {
-        setSelectedCategoriesList(selectedCategoriesList.filter((_, index) => index !== indexToRemove));
-    };
-
-    const handleRemoveColor = (indexToRemove) => {
-        setSelectedColorList(selectedColorsList.filter((_, index) => index !== indexToRemove));
-    };
-
+const Catalog = () => {
     const navigate = useNavigate();
-    const handleSubmit = () => {
+    const [selectcategory, setSelectCategory] = useState([]);
+    const [selectedColors, setSelectedColors] = useState([]);
+
+
+    const handleSubmit = (values, { setSubmitting }) => {
+        // Handle form submission logic here
         navigate("/products");
+        setSubmitting(false);
+    };
+    const option = [
+        { value: 'ROUND NACK', label: 'round nack' },
+        { value: 'POLO', label: 'polo' },
+        { value: 'V-NACK', label: 'v-nack' },
+    ]
+    const handleSelectCategory = (selectedOption) => {
+        setSelectCategory(selectedOption);
     };
 
-
-
+    const options = [
+        { value: 'RED', label: 'Red' },
+        { value: 'BLUE', label: 'Blue' },
+        { value: 'GREEN', label: 'Green' },
+    ]
+    const handleColorChange = (selectedOption) => {
+        setSelectedColors(selectedOption);
+    };
 
     return (
         <>
             <UserLayout />
-
             <div className='main layber'>
                 <div className='box_ps'>
-                    <form className="login100-form_0111">
-                        <span className="login100-form-title_034 p-b-43">
-                            ADD CATALOG
-                        </span>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            price: '',
+                            category: '',
+                            color: '',
+                            image: null
+                        }}
+                        validate={values => {
+                            // Implement your validation logic here
+                            const errors = {};
+                            return errors;
+                        }}
+                        onSubmit={handleSubmit}
+                    >
+                        {({ isSubmitting }) => (
+                            <Form className="login100-form_0111">
+                                <span className="login100-form-title_034 p-b-43">
+                                    ADD CATALOG
+                                </span>
 
-                        <div className='wrap-input100 validate-input'>
-                            <input className="input100" type="text" name="name" placeholder='PRODUCT NAME' />
-                            <span className="focus-input100"></span>
-                        </div>
-                        <br />
+                                <div className='wrap-input100 validate-input'>
+                                    <Field className="input100" type="text" name="name" placeholder='PRODUCT NAME' />
+                                    <span className="focus-input100"></span>
+                                </div>
 
-                        <CKEditor
-                            editor={ClassicEditor}
-                            data={editorData}
-                            onReady={handleReady}
-                            onChange={handleChange}
-                        />
-                        <br />
+                                <br />
+                                <CKEditor
+                                    editor={ClassicEditor}
+                                    data=""
+                                    onChange={(event, editor) => { }}
+                                />
+                                <br />
 
-                        <div className='wrap-input100 validate-input'>
-                            <input className="input100" type="number" name="price" placeholder='PRODUCT PRICE' />
-                            <span className="focus-input100"></span>
-                        </div>
+                                <div className='wrap-input100 validate-input'>
+                                    <Field className="input100" type="number" name="price" placeholder='PRODUCT PRICE' />
+                                    <span className="focus-input100"></span>
+                                </div>
 
-                        {/* <label htmlFor="category">CATEGORY : </label> */}
-                        <div className='wrap-input100 validate-input'>
-                            <select className="input100" id="category" name="category" onChange={handleCategoryChange} value={selectedCategory}>
-                                <option value="">Select a Category</option>
-                                <option value="ROUNDNACK">ROUND NACK</option>
-                                <option value="POLO">POLO</option>
-                                <option value="V-NACK">V-NACK</option>
-                                {/* Add more categories as needed */}
-                            </select>
-                            <span className="focus-input100"></span>
-                        </div>
-
-                        <div>
-                            <ul className="selected-categories">
-                                {selectedCategoriesList.map((category, index) => (
-                                    <li className='lidecore' key={index}>
-                                        {category}
-                                        <button className='close' onClick={() => handleRemoveCategory(index)}>x</button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                                <div>
+                                    <input type="text" name='category' />
+                                </div>
+                                <div className='wrap-input100 validate-input'>
+                                    <Select
+                                        className="input100"
+                                        options={option}
+                                        name="category"
+                                        placeholder="Select a category"
+                                        onChange={handleSelectCategory}
+                                        isMulti
+                                    />
+                                    <span className="focus-input100"></span>
+                                </div>
 
 
-                        <div className='wrap-input100 validate-input'>
-                            <select className="input100" id="color" name="color" onChange={handleColorChange} value={selectedColor}>
-                                <option value="">Select a Color</option>
-                                <option value="RED">RED</option>
-                                <option value="BLUE">BLUE</option>
-                                <option value="GREEN">GREEN</option>
-                                {/* Add more colors as needed */}
-                            </select>
-                            <span className="focus-input100"></span>
-                        </div>
-                        <div>
-                            {/* <p>Selected Color</p> */}
-                            <ul className="selected-categories">
-                                {selectedColorsList.map((color, index) => (
-                                    <li className='lidecore' key={index}>
-                                        {color}
-                                        <button className='close' onClick={() => handleRemoveColor(index)}>x</button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                                <div className='wrap-input100 validate-input'>
+                                    <Select
+                                        className="input100"
+                                        options={options}
+                                        name="color"
+                                        placeholder="Select a Color"
+                                        onChange={handleColorChange}
+                                        isMulti
+                                    />
+                                    <span className="focus-input100"></span>
+                                </div>
 
-                        <div className='wrap-input100 validate-input'>
-                            <input type="file" className="input100 imagecen" id="image" name="image" accept="image/*" onChange={handleImageChange} />
-                            <span className="focus-input100"></span>
-                        </div>
 
-                        {imageUrl && (
-                            <div className='image-preview '>
-                                <img src={imageUrl} className='imagesize' alt='Selected' />
-                            </div>
+                                {/* <div className="selected-colors">
+                                    <p>Selected Colors:</p>
+                                    {selectedColors.map((color, index) => (
+                                        <span key={index} className="selected-color">{color.label}</span>
+                                    ))}
+                                </div> */}
+
+
+                                <div className='wrap-input100 validate-input'>
+                                    <Field type="file" className="input100 imagecen" id="image" name="image" accept="image/*" />
+                                    <span className="focus-input100"></span>
+                                </div>
+
+
+                                <div className="container-login100-form-btn">
+                                    <button type="submit" className="login100-form-btn_confrom01 subtext" disabled={isSubmitting}>
+                                        {isSubmitting ? 'Submitting' : 'SUBMIT'}
+                                    </button>
+                                </div>
+                            </Form>
                         )}
-
-                        <div className="container-login100-form-btn">
-                            <button className="login100-form-btn_confrom01 subtext" onClick={handleSubmit}>
-                                SUBMIT
-                            </button>
-                        </div>
-                    </form>
+                    </Formik>
                 </div >
             </div >
         </>
     );
 }
+
+export default Catalog;
